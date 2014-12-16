8d9182a3da37c91d6ee8e70dac6b7ecb3995a528
Vasyl Harasymiv <vasyl.harasymiv@gmail.com>
2014-12-16 12:51:46 -0600

    Create AnagramFunction.r

---
+++ AnagramFunction.r
@@ -0,0 +1,17 @@
+anagram <- function(arg){
+   
+c=unique(unlist(strsplit(tolower(arg), " ")))
+d=sapply(lapply(strsplit(c, NULL), sort), paste, collapse="")
+
+e=data.frame(table(d))
+e=subset(e, Freq >1)
+
+s<-lapply(e$d, function(x) { c[which = d %in% x] })
+
+df<-do.call("rbind",s)
+
+
+       return(s)
+   }
+   
+#Test sentence: "I may opt for a top yam for amy amy"   
